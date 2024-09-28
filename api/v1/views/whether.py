8e767033e904808df1c7f0c4c49f467235844405
Oxshady <shadymahmod55@gmail.com>
2024-09-28 20:16:09 +0300

    views func

---
+++ api/v1/views/whether.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import requests
+import json
+
+url = "http://api.weatherapi.com/v1/current.json"
+params={"key":"c0aba767681e4adcaa0160828242809",
+        "q":"London"}
+
+response = requests.get(url, params=params)
+data = response.json()
+print(data)
+# /api/v1/categories
+# /api/v1/categories/events
+# /api/v1/enroll
+# /api/v1/myevents
+# /api/v1/auth/reg
+# /api/v1/auth/login
+# /api/v1/auth/logout
