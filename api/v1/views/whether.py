cd585a3b359c576e852e2fa3a004efebeacbc1a2
Oxshady <shadymahmod55@gmail.com>
2024-09-28 20:22:14 +0300

    static dir

--- api/v1/views/whether.py
+++ api/v1/views/whether.py
@@ -6,10 +6,10 @@ import json
 url = "http://api.weatherapi.com/v1/current.json"
 params={"key":"c0aba767681e4adcaa0160828242809",
         "q":"London"}
-
-response = requests.get(url, params=params)
-data = response.json()
-print(data)
+def whether():
+	response = requests.get(url, params=params)
+	data = response.json()
+	return data
 # /api/v1/categories
 # /api/v1/categories/events
 # /api/v1/enroll
